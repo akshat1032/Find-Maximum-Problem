c3ed33f096c45b8a7439c5ce5e4855536cb9df27
akshat1032 <akshat.30oct@gmail.com>
2020-10-08 21:21:08 +0530

    Added : Find maximum of three floating numbers added

--- FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
@@ -6,10 +6,23 @@ public class FindMaxProblem {
 
 	private static final Scanner INPUT_USER = new Scanner(System.in);
 	private static Integer maximumInt;
+	private static Double maximumFloat;
 
-	// Method to find maximum integer
+	// Method to find highest integer
 	public static int findMaxInteger(Integer first, Integer second, Integer third) {
 		Integer max = first;
+		if (second.compareTo(max) > 0) {
+			max = second;
+		}
+		if (third.compareTo(max) > 0) {
+			max = third;
+		}
+		return max;
+	}
+
+	// Method to find highest floating number
+	public static double findMaxFloat(Double first, Double second, Double third) {
+		Double max = first;
 		if (second.compareTo(max) > 0) {
 			max = second;
 		}
@@ -29,8 +42,21 @@ public class FindMaxProblem {
 		int second = INPUT_USER.nextInt();
 		int third = INPUT_USER.nextInt();
 
+		// Taking input for floating numbers from user
+		System.out.println("Enter three floating point numbers to check for maximum");
+		double firstFloat = INPUT_USER.nextInt();
+		double secondFloat = INPUT_USER.nextInt();
+		double thirdFloat = INPUT_USER.nextInt();
+
 		// Calling finding maximum integer function
 		maximumInt = findMaxInteger(first, second, third);
-		System.out.println("The largest integer out of " + first + ", " + second + ", " + third + " is : " + maximumInt);
+		System.out
+				.println("The largest integer out of " + first + ", " + second + ", " + third + " is : " + maximumInt);
+
+		// Calling finding maximum integer function
+		maximumFloat = findMaxFloat(firstFloat, secondFloat, thirdFloat);
+		System.out.println("The largest floating number out of " + firstFloat + ", " + secondFloat + ", " + thirdFloat
+				+ " is : " + maximumFloat);
+
 	}
 }
