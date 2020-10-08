ac508e98cba2e0035f122e5465db8c59756e9614
akshat1032 <akshat.30oct@gmail.com>
2020-10-08 21:33:31 +0530

    Added : Test for maximum floating number at position 2 added

--- FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblemFloatTest.java
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblemFloatTest.java
@@ -8,9 +8,17 @@ public class FindMaxProblemFloatTest {
 
 	// Test Case 2.1 Maximum number at position 1
 	@Test
-	public void testFindMaxIntegerOne() {
+	public void testFindMaxFloatOne() {
 		Double expectedValue = 9.9;
 		Double actualValue = FindMaxProblem.findMaxFloat(9.9, 9.6, 9.8);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 2.2 Maximum number at position 2
+	@Test
+	public void testFindMaxFloatTwo() {
+		Double expectedValue = 9.9;
+		Double actualValue = FindMaxProblem.findMaxFloat(9.6, 9.9, 9.8);
 		Assert.assertEquals(expectedValue, actualValue);
 	}
 
