d8146f6e7b33c1680e3845418606dc091accc3cf
akshat1032 <akshat.30oct@gmail.com>
2020-10-08 19:29:48 +0530

    Added : Welcome message added

---
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
@@ -0,0 +1,9 @@
+package com.capgemini.findmaxproblem;
+
+public class FindMaxProblem {
+
+	public static void main(String[] args) {
+		// Welcome message added
+		System.out.println("Welcome to find maximum problem");
+	}
+}
