2c4e2192a426bc09974f2af10268d90b534467e4
akshat1032 <akshat.30oct@gmail.com>
2020-10-08 23:24:09 +0530

    Refactor : Find maximum and test for maximum using generic classes refactored

--- FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
@@ -2,12 +2,19 @@ package com.capgemini.findmaxproblem;
 
 import java.util.Scanner;
 
-public class FindMaxProblem {
+public class FindMaxProblem<E extends Comparable<E>> {
 
 	private static final Scanner INPUT_USER = new Scanner(System.in);
-	
+	private E first, second, third;
+
+	public FindMaxProblem(E first, E second, E third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
 	// Generic Method to find max
-	public static <E extends Comparable <E>> E findMax( E first, E second, E third) {
+	public static <E extends Comparable<E>> E findMax(E first, E second, E third) {
 		E max = first;
 		if ((second.compareTo(max)) > 0) {
 			max = second;
@@ -16,6 +23,11 @@ public class FindMaxProblem {
 			max = third;
 		}
 		return max;
+	}
+
+	// Method to internally call static find max method
+	public E testMaximum() {
+		return findMax(this.first, this.second, this.third);
 	}
 
 	public static void main(String[] args) {
@@ -35,24 +47,25 @@ public class FindMaxProblem {
 		double secondFloat = INPUT_USER.nextDouble();
 		double thirdFloat = INPUT_USER.nextDouble();
 		INPUT_USER.nextLine();
-		
+
 		// Taking input for strings from user
 		System.out.println("Enter three strings to check for maximum");
 		String firstString = INPUT_USER.nextLine();
 		String secondString = INPUT_USER.nextLine();
 		String thirdString = INPUT_USER.nextLine();
-		
+
 		// Calling find max function for different types
-		Integer maximumInt = findMax(first, second, third);
-		Double maximumDouble = findMax(firstFloat,secondFloat,thirdFloat);
-		String maximumString = findMax(firstString, secondString, thirdString);
-		
-		//Printing the maximum to console
+		Integer maximumInt = new FindMaxProblem<Integer>(first, second, third).testMaximum();
+		Double maximumDouble = new FindMaxProblem<Double>(firstFloat, secondFloat, thirdFloat).testMaximum();
+		String maximumString = new FindMaxProblem<String>(firstString, secondString, thirdString).testMaximum();
+
+		// Printing the maximum to console
 		System.out
-		.println("The largest integer out of " + first + ", " + second + ", " + third + " is : " + maximumInt);
-		System.out
-		.println("The largest floating number out of " + firstFloat + ", " + secondFloat + ", " + thirdFloat + " is : " + maximumDouble);
-		System.out
-		.println("The largest String out of " + firstString + ", " + secondString + ", " + thirdString + " is : " + maximumString);
+				.println("The largest integer out of " + first + ", " + second + ", " + third + " is : " + maximumInt);
+		System.out.println("The largest floating number out of " + firstFloat + ", " + secondFloat + ", " + thirdFloat
+				+ " is : " + maximumDouble);
+		System.out.println("The largest String out of " + firstString + ", " + secondString + ", " + thirdString
+				+ " is : " + maximumString);
 	}
+
 }
---
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblemGenericTest.java
@@ -0,0 +1,80 @@
+package com.capgemini.findmaxproblem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindMaxProblemGenericTest {
+
+	// Test Case 1.1 Maximum number at position 1
+	@Test
+	public void testFindMaxIntegerOne() {
+		Integer expectedValue = 9;
+		Integer actualValue = new FindMaxProblem<Integer>(9, 6, 8).testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 1.2 Maximum number at position 2
+	@Test
+	public void testFindMaxIntegerTwo() {
+		Integer expectedValue = 9;
+		Integer actualValue = new FindMaxProblem<Integer>(6, 9, 8).testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 1.3 Maximum number at position 3
+	@Test
+	public void testFindMaxIntegerThree() {
+		Integer expectedValue = 9;
+		Integer actualValue = new FindMaxProblem<Integer>(7, 6, 9).testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 2.1 Maximum number at position 1
+	@Test
+	public void testFindMaxFloatOne() {
+		Double expectedValue = 9.9;
+		Double actualValue = new FindMaxProblem<Double>(9.9, 9.6, 9.8).testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 2.2 Maximum number at position 2
+	@Test
+	public void testFindMaxFloatTwo() {
+		Double expectedValue = 9.9;
+		Double actualValue = new FindMaxProblem<Double>(9.6, 9.9, 9.8).testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 2.3 Maximum number at position 3
+	@Test
+	public void testFindMaxFloatThree() {
+		Double expectedValue = 9.9;
+		Double actualValue = new FindMaxProblem<Double>(9.7, 9.6, 9.9).testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 3.1 Maximum String at position 1
+	@Test
+	public void testFindMaxStringOne() {
+		String expectedValue = "Peach";
+		String actualValue = new FindMaxProblem<String>("Peach", "Apple", "Banana").testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 3.2 Maximum String at position 2
+	@Test
+	public void testFindMaxStringTwo() {
+		String expectedValue = "Peach";
+		String actualValue = new FindMaxProblem<String>("Apple", "Peach", "Banana").testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 3.3 Maximum String at position 3
+	@Test
+	public void testFindMaxStringThree() {
+		String expectedValue = "Peach";
+		String actualValue = new FindMaxProblem<String>("Apple", "Banana", "Peach").testMaximum();
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+}
