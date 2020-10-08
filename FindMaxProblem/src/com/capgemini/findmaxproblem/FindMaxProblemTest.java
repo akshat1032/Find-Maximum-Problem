ae1da89932d830d3a06a4145ab0920e59128f12c
akshat1032 <akshat.30oct@gmail.com>
2020-10-08 21:04:40 +0530

    Added : Test for maximum integer at position 2 added

--- FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblemTest.java
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblemTest.java
@@ -6,11 +6,19 @@ import org.junit.Test;
 
 public class FindMaxProblemTest {
 
-	// Test Case 1.1 Maximum number at position 1	
+	// Test Case 1.1 Maximum number at position 1
 	@Test
-	public void testFindMaxInteger() {
+	public void testFindMaxIntegerOne() {
 		Integer expectedValue = 9;
 		Integer actualValue = FindMaxProblem.findMaxInteger(9, 6, 8);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+	// Test Case 1.2 Maximum number at position 2
+	@Test
+	public void testFindMaxIntegerTwo() {
+		Integer expectedValue = 9;
+		Integer actualValue = FindMaxProblem.findMaxInteger(6, 9, 8);
 		Assert.assertEquals(expectedValue, actualValue);
 	}
 
