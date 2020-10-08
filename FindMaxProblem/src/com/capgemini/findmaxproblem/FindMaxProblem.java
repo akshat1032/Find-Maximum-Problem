0ea43658e616cee08dc04c893adfa067a99b1993
akshat1032 <akshat.30oct@gmail.com>
2020-10-08 21:49:49 +0530

    Added : Find maximum of three strings added

--- FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
+++ FindMaxProblem/src/com/capgemini/findmaxproblem/FindMaxProblem.java
@@ -7,6 +7,7 @@ public class FindMaxProblem {
 	private static final Scanner INPUT_USER = new Scanner(System.in);
 	private static Integer maximumInt;
 	private static Double maximumFloat;
+	private static String maximumString;
 
 	// Method to find highest integer
 	public static int findMaxInteger(Integer first, Integer second, Integer third) {
@@ -32,6 +33,18 @@ public class FindMaxProblem {
 		return max;
 	}
 
+	// Method to find highest string
+	public static String findMaxString(String first, String second, String third) {
+		String max = first;
+		if (second.compareTo(max) > 0) {
+			max = second;
+		}
+		if (third.compareTo(max) > 0) {
+			max = third;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		// Welcome message added
 		System.out.println("Welcome to find maximum problem\n");
@@ -44,19 +57,29 @@ public class FindMaxProblem {
 
 		// Taking input for floating numbers from user
 		System.out.println("Enter three floating point numbers to check for maximum");
-		double firstFloat = INPUT_USER.nextInt();
-		double secondFloat = INPUT_USER.nextInt();
-		double thirdFloat = INPUT_USER.nextInt();
+		double firstFloat = INPUT_USER.nextDouble();
+		double secondFloat = INPUT_USER.nextDouble();
+		double thirdFloat = INPUT_USER.nextDouble();
+
+		// Taking input for strings from user
+		System.out.println("Enter three strings to check for maximum");
+		String firstString = INPUT_USER.nextLine();
+		String secondString = INPUT_USER.nextLine();
+		String thirdString = INPUT_USER.nextLine();
 
 		// Calling finding maximum integer function
 		maximumInt = findMaxInteger(first, second, third);
 		System.out
 				.println("The largest integer out of " + first + ", " + second + ", " + third + " is : " + maximumInt);
 
-		// Calling finding maximum integer function
+		// Calling finding maximum floating number function
 		maximumFloat = findMaxFloat(firstFloat, secondFloat, thirdFloat);
 		System.out.println("The largest floating number out of " + firstFloat + ", " + secondFloat + ", " + thirdFloat
 				+ " is : " + maximumFloat);
-
+		
+		// Calling finding maximum String function
+				maximumString = findMaxString(firstString, secondString, thirdString);
+				System.out.println("The largest String out of " + firstString + ", " + secondString + ", " + thirdString
+						+ " is : " + maximumString);
 	}
 }
